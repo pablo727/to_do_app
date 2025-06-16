267debe7add460910817ec146a2b5471e762a6dd
Pablo Rodriguez Sanz <pablo.rodrguezsanz@yahoo.com>
2025-06-16 23:47:59 +0800

    permissions and authorizations

--- task_scheduler/views.py
+++ task_scheduler/views.py
@@ -1,20 +1,21 @@
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import UpdateView, DeleteView, CreateView
 from django.urls import reverse_lazy
 from .models import TaskScheduler
 
 
-class TaskSchedulerListView(ListView):
+class TaskSchedulerListView(LoginRequiredMixin, ListView):
     model = TaskScheduler
     template_name = "taskscheduler_list.html"
 
 
-class TaskSchedulerDetailView(DetailView):
+class TaskSchedulerDetailView(LoginRequiredMixin, DetailView):
     model = TaskScheduler
     template_name = "taskscheduler_detail.html"
 
 
-class TaskSchedulerUpdateView(UpdateView):
+class TaskSchedulerUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = TaskScheduler
     fields = (
         "title",
@@ -24,14 +25,22 @@ class TaskSchedulerUpdateView(UpdateView):
     )
     template_name = "taskscheduler_edit.html"
 
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
 
-class TaskSchedulerDeleteView(DeleteView):
+
+class TaskSchedulerDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = TaskScheduler
     template_name = "taskscheduler_delete.html"
     success_url = reverse_lazy("taskscheduler_list")
 
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
 
-class TaskSchedulerCreateView(CreateView):
+
+class TaskSchedulerCreateView(LoginRequiredMixin, CreateView):
     model = TaskScheduler
     template_name = "taskscheduler_new.html"
     fields = (
@@ -39,5 +48,8 @@ class TaskSchedulerCreateView(CreateView):
         "description",
         "scheduled_time",
         "completed",
-        "author",
     )
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
--- tasks/views.py
+++ tasks/views.py
@@ -1,32 +1,45 @@
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .models import TaskItem
 
 
-class TasksListView(ListView):
+class TasksListView(LoginRequiredMixin, ListView):
     model = TaskItem
     template_name = "home.html"
 
 
-class TasksDetailView(DetailView):
+class TasksDetailView(LoginRequiredMixin, DetailView):
     model = TaskItem
     template_name = "task_detail.html"
 
 
-class TasksCreateView(CreateView):
+class TasksCreateView(LoginRequiredMixin, CreateView):
     model = TaskItem
     template_name = "task_new.html"
-    fields = ["title", "author", "description"]
+    fields = ["title", "description"]
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 
-class TasksUpdateView(UpdateView):
+class TasksUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = TaskItem
     template_name = "task_edit.html"
     fields = ["title", "description"]
 
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
 
-class TasksDeleteView(DeleteView):
+
+class TasksDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = TaskItem
     template_name = "task_delete.html"
     success_url = reverse_lazy("home")
+
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
